002d54dfdf133113e2e6d3fc5f89cd167a49567c
Odyn666 <lambertmorhen@wp.pl>
2023-10-26 18:53:33 +0200

    2Dpoint Task added

---
+++ src/main/java/pl/hajduk/task9/Circle.java
@@ -0,0 +1,38 @@
+package pl.hajduk.task9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Następnie stwórz klasę Circle, która będzie
+// miała konstruktor: Circle(Point2D center, Point2D point)
+//Pierwszy parametr określa środek koła, drugi jest dowolnym punktem na okręgu.
+// Na podstawie tych punktów, klasa Circle ma określać:
+//• promień okręgu przy wywołaniu metody double getRadius()
+//• obwód okręgu przy wywołaniu metody double getPerimeter()
+//• pole okręgu przy wywołaniu metody double getArea()
+//• (trudniejsze) trzy punkty na okręgu co 90 stopni od punktu podanego przy wywołaniu metody List getSlicePoints()
+public class Circle {
+    Point2D center;
+    Point2D point;
+
+    public Circle(Point2D center, Point2D point) {
+
+        this.center=center;
+        this.point=point;
+    }
+
+    double getRadius() {
+        double dx = center.getX() - point.getX();
+        double dy = center.getY() - point.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    double getPerimeter() {
+        return 2*Math.PI*getRadius();
+
+    }
+
+    double getArea() {
+        return Math.PI*(getRadius()*getRadius());
+    }
+}
